c6d7784e4255e702398bf3146eff4932e1ee1edd
mina <minaduranovic@gmail.com>
2024-01-07 00:03:34 +0100

    glavnaCtrl buttons

--- src/main/java/ba/unsa/etf/rpr/GeoApp.java
+++ src/main/java/ba/unsa/etf/rpr/GeoApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GeoApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("glavna.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GeoApp.class.getResource("glavna.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 538, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gradovi");
         stage.setScene(scene);
         stage.show();
     }
