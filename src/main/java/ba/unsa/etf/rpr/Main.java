769e8d8632f0d65f5a39d49c9d257d26a04974cc
mina <minaduranovic@gmail.com>
2024-01-10 19:52:13 +0100

    main

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -7,8 +7,6 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import java.io.IOException;
 
-
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
